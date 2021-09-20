c0338de7099ba81638db3dbe357713e2ccf4c0ac
subbarajpenmetsa <subbarajpenmetsa@gmail.com>
2021-09-20 13:02:13 +0530

    Update demo_rds.tf

--- demo_rds.tf
+++ demo_rds.tf
@@ -5,29 +5,12 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   tags = { Name = "RDS DB subnet group" }
 }
 
+# Retriving DB credentials from AWS Secret Manager
 
-# resource "random_password" "master"{
-#   length           = 16
-#   special          = true
-#   override_special = "_!%^"
-# }
+data "aws_secretsmanager_secret_version" "username" {
+  secret_id = "db-username"
 
-# resource "aws_secretsmanager_secret" "password" {
-#   name = "test-db-password"
-# }
-
-# resource "aws_secretsmanager_secret_version" "password" {
-#   secret_id = aws_secretsmanager_secret.password.id
-#   secret_string = {
-#     password= random_password.master.result}
-# }
-
-# resource "aws_secretsmanager_secret_version" "example" {
-#   secret_id     = aws_secretsmanager_secret.example.id
-#   secret_string = jsonencode(var.example)
-# }
-
-# Retriving DB password from AWS Secret Manager
+}
 
 data "aws_secretsmanager_secret_version" "password" {
   secret_id = "db-password"
@@ -44,7 +27,7 @@ resource "aws_db_instance" "rds" {
   name                   = "mydb"
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.id
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
-  username               = var.rds_admin
+  username               = local.db_username.username
   password               = local.db_creds.password
 
   parameter_group_name = "default.mysql5.7"
